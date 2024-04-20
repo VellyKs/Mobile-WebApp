f8f2c840d75539ef987772b3d8d98bcb9facc8b4
Ppedro.Leal <111246857+Orezo@users.noreply.github.com>
2024-04-20 17:52:39 -0300

    rename pasta index

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(path.join(__dirname, "docs", "index.html"));
 });
 
 app.get("/diario", async (req, res) => {
